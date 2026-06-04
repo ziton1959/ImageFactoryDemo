e784fa64f6fe0ebcd68d70398481a6a9e0fabc74
zitouni <essra.zitouni@hpe.com>
2026-06-04 18:40:40 +0100

    fix origin

--- next.user-config.mjs
+++ next.user-config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: ['10.202.135.233'],
+
   async rewrites() {
     return [
       {
         source: "/api/:path*",
         destination: "http://127.0.0.1:8000/api/:path*",
       },
-    ]
+    ];
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
